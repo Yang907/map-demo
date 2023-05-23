bddb7f099ef736f34ab8fd467070a4bb46fd997a
yangbing <yangbing@adas.com>
2023-05-23 09:16:02 +0800

    feat:上传打包文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/map-demo/",
+  build: {
+    outDir: "docs",
+  },
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
